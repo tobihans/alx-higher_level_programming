70ff0e22bc4c186073f771eaa00233f7f29976fe
Hans Tognon <learningway403@gmail.com>
2022-10-03 12:05:19 +0100

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for n in range(99):
-    print("{:0>2}".format(n), end=", " if n < 98 else "\n")
+for n in range(100):
+    print("{:0>2}".format(n), end=", " if n < 99 else "\n")
