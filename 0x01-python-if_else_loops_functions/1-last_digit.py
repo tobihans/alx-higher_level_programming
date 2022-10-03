4b4fbcc1de9bfe1d577104220313e86eab35057e
Hans Tognon <learningway403@gmail.com>
2022-10-03 11:28:54 +0100

    fix: bad string format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ last_digit = abs(number) % 10
 prefix = f"Last digit of {number} is {last_digit} and is"
 
 if number == 0:
-    print(f"{number} is 0")
+    print(f"{prefix} is 0")
 elif last_digit > 5:
     print(f"{prefix} greater than 5")
 elif number < 6 and number != 0:
-    print(f"{number} less than 6 and not 0")
+    print(f"{prefix} less than 6 and not 0")
