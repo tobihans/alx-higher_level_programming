200798bf82f092f2d49d656524e43ca0ee2cdbdd
Hans Tognon <learningway403@gmail.com>
2022-10-03 11:24:00 +0100

    0x01,fix: fix conditions order

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,9 @@ number = random.randint(-10, 10)
 last_digit = number % 10
 prefix = f"Last digit of {number} is {last_digit} and is"
 
-if last_digit > 5:
+if number == 0:
+    print(f"{number} is 0")
+elif last_digit > 5:
     print(f"{prefix} greater than 5")
 elif number < 6 and number != 0:
     print(f"{number} less than 6 and not 0")
-else:
-    print(f"{number} is 0")
-
