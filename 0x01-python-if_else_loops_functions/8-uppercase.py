a1c261076bb34c9ed451d62486bc0a202bf788b9
Hans Tognon <learningway403@gmail.com>
2022-10-03 12:42:58 +0100

    fix: format and range

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,7 @@
 
 def uppercase(str):
     for c in str:
-        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+        print(
+            "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end=""
+        )
     print("")
