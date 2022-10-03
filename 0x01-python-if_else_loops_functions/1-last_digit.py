ce9818efb4c20920d6bfaaba14bd3c660f3c8249
Hans Tognon <learningway403@gmail.com>
2022-10-03 11:36:01 +0100

    fix: handle negative properly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = (abs(number) % 10) * (-1 if number < 0 else 1)
 prefix = f"Last digit of {number} is {last_digit} and is"
 
 if number == 0:
