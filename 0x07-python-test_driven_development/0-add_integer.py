46c2add6ca2613eb353e91d58732f084b8761e34
Hans Tognon <learningway403@gmail.com>
2022-10-25 06:58:01 +0100

    late but I have to do it

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+A module for addition.
+
+It provides functions to perfom addition operations.
+
+That's it.
+"""
+
+def add_integer(a, b=98):
+  """Sums two int or floats together.
+
+  A TypeError will be raised if one the arguments is not an int or a float
+  """
+
+  if not isinstance(a, (int, float)):
+    raise TypeError("a must be an integer")
+
+  if not isinstance(b, (int, float)):
+    raise TypeError("b must be an integer")
+
+  return int(a) + int(b)
